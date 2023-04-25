de71bf4dd3f4fe496976b189ff770bfaeee4896b
Armelio <54901926+Armelio@users.noreply.github.com>
2023-04-25 15:31:10 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,10 @@
 async function getData() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts/1", 
+  {
+    headers:{
+      "Access-Control-Allow-Origin": "*"
+    }
+  });
   const data = await response.json();
   console.log(data);
 }
