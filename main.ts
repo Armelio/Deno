31d042a267912cd46918ffa197dcaf18c00885e2
Armelio <54901926+Armelio@users.noreply.github.com>
2023-04-25 15:43:20 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ async function getData() {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts/1", 
   {
     headers:{
-      "Origin": "http://127.0.0.1:5501'"
+      "Origin": "http://127.0.0.1:5501"
     }
   });
   const data = await response.json();
