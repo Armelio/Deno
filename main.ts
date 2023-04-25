73988fb48680f49d9dcf101b7fbd36a0f6bf661e
Armelio <54901926+Armelio@users.noreply.github.com>
2023-04-25 17:43:36 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
-const res = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+const res = await fetch("https://jsonplaceholder.typicode.com/posts/1", {
+  headers: {
+    "Origin": "*"
+  }
+});
 const body = await res.text();
 console.log(body);
 
